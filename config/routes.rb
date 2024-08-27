e8e1dc6d8f351398bb34891ad743ad42c2d450cf
Corentin Dubois <corentindubois22@gmail.com>
2024-08-27 12:56:54 -0600

    done

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,17 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+  patch "tasks/:id", to: "tasks#update", as: :task
 
   # Render dynamic PWA files from app/views/pwa/*
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+  get "tasks" => "tasks#display"
+  get "tasks/new" => "tasks#new"
+  post "tasks" => "tasks#create"
+  get "tasks/:id" => "tasks#details"
+  get "tasks/:id/edit" => "tasks#edit"
+  delete "tasks/:id" => "tasks#destroy"
 
   # Defines the root path route ("/")
   # root "posts#index"
